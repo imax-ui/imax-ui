8fb686c4475f4eb5b2b127a2eacde02c29d0b42e
zhijiang3 <zhijiang34@gmail.com>
2019-02-23 12:42:32 +0800

    add scss mixins

--- packages/components/alert/src/main.vue
+++ packages/components/alert/src/main.vue
@@ -1,15 +1,15 @@
 <template>
-  <transition name="imax__alert-fade">
-    <div class="imax__alert">
-      <div class="imax__alert__header">
-        <div class="imax__alert--title">
+  <transition name="fade-in-top">
+    <div class="imax-alert">
+      <div class="imax-alert__header">
+        <div class="imax-alert--title">
           {{ title }}
         </div>
       </div>
-      <div class="imax__alert__inner">
+      <div class="imax-alert__inner">
         {{ message }}
       </div>
-      <div class="imax__alert__confirm-text">
+      <div class="imax-alert__confirm-text">
         {{ confirmText }}
       </div>
     </div>
